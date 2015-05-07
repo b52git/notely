8280efc92965db34654107ac5ca99adc1862e66b
Edward Rich <edwardwrich@gmail.com>
2015-05-07 14:01:39 -0400

    Require authentication on everything

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,12 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   private
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def authorize_user
+    redirect_to login_path, alert: t('user.flash.unauthorized') if current_user.nil?
   end
 end
