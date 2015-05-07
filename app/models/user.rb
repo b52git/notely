c841cdee72964f8638619e2b2e973e52d8b9a926
Edward Rich <edwardwrich@gmail.com>
2015-05-07 11:12:09 -0400

    Added relationship between user and note models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
   has_secure_password
+  has_many :notes
   validates :password, length: { minimum: 8 }
+  validate :require_password_confirmation
+
+  private
+    def require_password_confirmation
+      if password.present? && password != password_confirmation
+        errors.add :password_confirmation, 'is required when setting your password'
+      end
+  end
 end
